70020a13ba9719e6bc8fb410f0defe77ecc2484e
Hatef Palizgar <hatef.palizgar@gamesys.co.uk>
2020-11-15 09:59:33 +0200

    exercise pocket

--- src/com/sda/java/encapsulation/Demo.java
+++ src/com/sda/java/encapsulation/Demo.java
@@ -3,15 +3,12 @@ package com.sda.java.encapsulation;
 public class Demo {
     
     public static void main(String[] args) {
-        Dog aDog = new Dog();
-        Dog bDog = new Dog('f',"Bulldog");
-        Dog cDog = new Dog("Pug", 1, 'f', "Pitbull", 2.5);
-        System.out.println("====== aDog ======");
-        System.out.println(aDog.toString());
-        System.out.println("====== bDog ======");
-        System.out.println(bDog.toString());
-        System.out.println("====== cDog ======");
-        System.out.println(cDog.toString());
-    
+        Pocket aPocket = new Pocket(4000);
+        System.out.println(aPocket.getMoney());
+        aPocket.setMoney(5000);
+        System.out.println(aPocket.getMoney());
+       
+        Pocket bPocket = new Pocket(5);
+        System.out.println(bPocket.getMoney());
     }
 }
--- src/com/sda/java/encapsulation/Pocket.java
+++ src/com/sda/java/encapsulation/Pocket.java
@@ -1,2 +1,24 @@
-package com.sda.java.encapsulation;public class Pocket {
+package com.sda.java.encapsulation;
+
+public class Pocket {
+    private double money;
+    
+    public Pocket(double money) {
+        this.money = money;
+    }
+    
+    public double getMoney() {
+        if(money <= 10){
+            return 0;
+        }
+        return money;
+    }
+    
+    public void setMoney(double money) {
+        if(money < 0 || money > 3000){
+            System.out.println("I do not have enough space in my pocket for as much money");
+        }else{
+            this.money = money;
+        }
+    }
 }
